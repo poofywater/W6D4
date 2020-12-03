03ff3fa2b7e2c5cdd99e88565dbddf7165c72a2d
Derek Chen <derekchen72@gmail.com>
2020-12-03 12:11:06 -0500

    routes.rb

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :users
+  get 'users', to: 'users#index'
+  post 'users', to: 'users#create'
+
+  get 'users/:id', to: 'users/:id#show'
+  patch 'users/:id', to: 'users/:id#update'
+  put 'users/:id', to: 'users/:id#update'
+  delete 'users/:id', to: 'users/:id#destroy'
+ 
+  get 'users', to: 'users#new'
+  
+  get 'users', to: 'users#edit'
+
 end
